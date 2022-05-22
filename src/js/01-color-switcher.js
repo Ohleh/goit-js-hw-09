3559b919a5c31da217d84860e226a3620ebd63a5
Ohleh <oleg.kolischuk@gmail.com>
2022-05-22 21:43:05 +0300

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const ref = {
+  body: document.querySelector('body'),
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+ref.start.addEventListener('click', onStartClick);
+ref.stop.addEventListener('click', onStopClick);
+
+function onStartClick(action) {
+  timerId = setInterval(() => {
+    ref.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  ref.start.disabled = true;
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  ref.start.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
